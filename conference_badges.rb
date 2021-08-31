b41614746ed69a3495fb3bbd90a770b306dc32bc
Sam Shearer <sshearer101@gmail.com>
2021-08-31 10:22:52 -0600

    commit tree e1a16adc13880e0d3be69e7b2ba63d210931bdd4

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(people)
+    people.map do |name| 
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(speaker)
+    speaker.map.with_index(1) do |name, room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(people)
+    batch_badge_creator(people).each do |badge|
+        puts badge 
+    end
+
+    assign_rooms(people).each do |room|
+        puts room
+    end
+end 
